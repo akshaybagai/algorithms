9cdc330c486ea496b7cc12801fddbc624ef9b74d
akshaybagai <akshaybagai@gmail.com>
2020-08-02 12:56:39 -0400

    tp

--- src/trees/Tree.java
+++ src/trees/Tree.java
@@ -4,24 +4,27 @@ import com.sun.codemodel.internal.JForEach;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
 
 public class Tree {
 
     public static void main(String[] args) {
 
-        TreeNode<String> book = new TreeNode<>("Index");
+        TreeNode<String, Integer> book = new TreeNode<>("Index");
 
-        TreeNode<String> ch1 = new TreeNode<>("Chapter 1");
+        TreeNode<String, Integer> ch1 = new TreeNode<>("Chapter 1");
         ch1.insert("Section 1.1");
         ch1.insert("Section 1.2");
         ch1.insert("Section 1.3");
 
-        TreeNode<String> ch2 = new TreeNode<>("Chapter 2");
+        TreeNode<String, Integer> ch2 = new TreeNode<>("Chapter 2");
         ch2.insert("Section 2.1");
 
-        TreeNode<String> ch3 = new TreeNode<>("Chapter 3");
+        TreeNode<String, Integer> ch3 = new TreeNode<>("Chapter 3");
         ch3.insert("Section 3.1");
         ch3.insert("Section 3.2");
         ch3.insert("Section 3.3");
@@ -33,38 +36,60 @@ public class Tree {
         book.insert(ch3);
 
         book.printPreOrder();
+
+        TreeNode<String, Integer> root = new TreeNode<>("/", 0);
+
+        TreeNode<String, Integer> etc = new TreeNode<>("etc", 0);
+        etc.insert(new TreeNode<>("a", 5));
+        etc.insert(new TreeNode<>("b", 50));
+        etc.insert(new TreeNode<>("c", 3));
+
+        TreeNode<String, Integer> trees = new TreeNode<>("trees", 0);
+        trees.insert(new TreeNode<>("a", 2));
+        trees.insert(new TreeNode<>("b", 1));
+        trees.insert(new TreeNode<>("c", 4));
+        
+        root.insert(etc);
+        root.insert(trees);
+
+        System.out.println(TreeNode.sum(root));
     }
 
 
     
-    static class TreeNode<T> {
+    static class TreeNode<T, V> {
         T key;
-        Object value;
+        V value;
         //instead of left and right child in bst
-        List<TreeNode<T>> children = new ArrayList<>();
+        List<TreeNode<T, V>> children = new ArrayList<>();
 
 
         public TreeNode(T key) {
             this.key = key;
         }
 
-        public TreeNode<T> insert(TreeNode<T> key) {
+        public TreeNode(T key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public TreeNode<T,V> insert(TreeNode<T,V> key) {
             this.children.add(key);
             return this;
         }
 
-        public TreeNode<T> insert(T key) {
+        public TreeNode<T,V> insert(T key) {
             return this.insert(new TreeNode<>(key));
         }
 
-        public void levelOrder(TreeNode<T> root) {
-            Queue<TreeNode<T>> ts = new LinkedBlockingQueue<>();
+        public void levelOrder(TreeNode<T,V> root) {
+            Queue<TreeNode<T,V>> ts = new LinkedBlockingQueue<>();
             ts.add(root);
 
             while (!ts.isEmpty()) {
-                TreeNode<T> node = ts.poll();
+                TreeNode<T,V> node = ts.poll();
                 System.out.println(node);
-                for(TreeNode<T> child : node.children) {
+                for(TreeNode<T,V> child : node.children) {
                     ts.add(child);
                 }
             }
@@ -74,7 +99,7 @@ public class Tree {
             helper(this, 0);
         }
 
-        public void helper(TreeNode<T> node, int level) {
+        public void helper(TreeNode<T,V> node, int level) {
             if (node == null) return;
             int numberOfSpaces = level;
             //pre order + number of levels
@@ -85,9 +110,18 @@ public class Tree {
             }
             sb.append(node);
             System.out.println(sb.toString());
-            for (TreeNode<T> child: node.children) {
+            for (TreeNode<T,V> child: node.children) {
                 helper(child, level + 1);
             }
+        }
+
+        public static <T> Integer sum(TreeNode<T,Integer> node) {
+            if (node == null) return null;
+            Integer d = node.value;
+            for (TreeNode<T,Integer> child: node.children) {
+                d += sum(child);
+            }
+            return d;
         }
 
         @Override
